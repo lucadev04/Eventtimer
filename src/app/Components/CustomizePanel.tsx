1f00dfd9e6f23895f9c18e04da0d77629ba4e801
lucadev04 <luca07457@gmail.com>
2025-07-11 18:08:08 +0200

    created upload image and search image for background, added basic upload image function

--- src/app/Components/CustomizePanel.tsx
+++ src/app/Components/CustomizePanel.tsx
@@ -1,35 +1,79 @@
 "use client";
 
 import styles from "../page.module.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import UIkit from "uikit";
 
 const CustomizePanel = () => {
+  const inputFile = useRef<HTMLInputElement | null>(null);
   useEffect(() => {
-    // Manuelle Initialisierung (oft reicht das, UIkit erkennt den Switcher dann)
     UIkit.switcher(".uk-subnav");
   }, []);
-  return (
-    <div className={styles.panel}>
-      <center>
-        <ul className="uk-subnav uk-subnav-pill" uk-switcher>
-          <li>
-            <a href="#">Design</a>
-          </li>
-          <li>
-            <a href="#">Background</a>
-          </li>
-          <li>
-            <a href="#">Effects</a>
-          </li>
-        </ul>
 
-        <div className="uk-switcher uk-margin">
-          <div>Hello!</div>
-          <div>Hello again!</div>
-          <div>Bazinga!</div>
-        </div>
-      </center>
+  const uploadImage = () => {
+    if (inputFile.current) {
+      inputFile.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      console.log(imageUrl);
+    }
+  };
+
+  return (
+    <div className="uk-container">
+      <div className={styles.panel}>
+        <center>
+          <ul className="uk-subnav uk-subnav-pill" uk-switcher>
+            <li>
+              <a href="#">Design</a>
+            </li>
+            <li>
+              <a href="#">Background</a>
+            </li>
+            <li>
+              <a href="#">Effects</a>
+            </li>
+          </ul>
+
+          <div className="uk-switcher uk-margin">
+            <div>Design</div>
+            <div className="uk-flex uk-flex-column">
+              <button
+                className="uk-button uk-button-primary"
+                style={{
+                  width: "11em",
+                  marginBottom: "10px",
+                  marginLeft: "45%",
+                }}
+                onClick={uploadImage}
+              >
+                upload image
+              </button>
+              <input
+                type="file"
+                id="file"
+                ref={inputFile}
+                accept="image/*"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+              />
+              <a
+                className="uk-button uk-button-primary"
+                href=""
+                style={{ width: "11em", marginLeft: "45%" }}
+              >
+                search images
+              </a>
+            </div>
+            <div>Effects</div>
+          </div>
+        </center>
+      </div>
     </div>
   );
 };
